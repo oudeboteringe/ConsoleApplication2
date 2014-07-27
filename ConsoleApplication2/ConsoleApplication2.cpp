1aab70e42bdb7aec946eaeede068ecd65fca5f4d
oudeboteringe <michiel_72@hotmail.com>
2014-07-27 20:59:50 +0200

    test
    added: _getch() statement

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -2,10 +2,15 @@
 //
 
 #include "stdafx.h"
+#include <conio.h>
+#include <iostream>
+using namespace std;
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	cout << "Console2";
+	_getch();
 	return 0;
 }
 
